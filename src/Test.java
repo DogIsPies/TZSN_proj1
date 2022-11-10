426eb11b8cd2ce12776f56c5f53cea6599db11fd
Vesemi <czyzewski.kamil@outlook.com>
2022-11-10 12:25:53 +0100

    GUI elemensts moved to separate file

--- src/Test.java
+++ src/Test.java
@@ -43,86 +43,8 @@ public class Test extends JFrame{
 	}
 	public Test(String string) {
 		super(string);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(new BorderLayout());
-		Toolkit kit=Toolkit.getDefaultToolkit();
-		Dimension d=kit.getScreenSize();
-		setBounds(d.width/4,d.height/4,d.width/2,d.height/2);
-		JSlider sliderSlices = new JSlider(JSlider.HORIZONTAL, 8,64, 8);
+		new UI();
 
-		JPanel panelRight = new JPanel();
-		JPanel panelLeft = new JPanel(new BorderLayout());
-		JPanel panelDrawing = new JPanel(new BorderLayout());
-		panelDrawing.setBounds(0, 0, 100, 100);
-		//drawingArea = new DrawArea();
-		drawingArea = new DrawArea();
-		drawingArea.setBounds(0,0,200,200);
-
-		panelDrawing.setBorder(new EmptyBorder(100, 100, 100, 100));
-		panelDrawing.add(drawingArea, BorderLayout.CENTER);
-		panelLeft.add(panelDrawing, BorderLayout.CENTER);
-		panelLeft.add(sliderSlices, BorderLayout.SOUTH);
-
-		JButton btnTeach = new JButton("Teach");
-		JButton btnSave = new JButton("Save");
-		JButton btnEdit = new JButton("Edit");
-		JButton btnLoad = new JButton("Load");
-		JButton btnClear = new JButton("Clear");
-		btnClear.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				drawingArea.clear();
-			}
-		});
-
-		btnSave.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					drawingArea.save();
-				} catch (IOException ex) {
-					throw new RuntimeException(ex);
-				}
-			}
-		});
-
-		btnLoad.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					drawingArea.load();
-				} catch (IOException ex) {
-					throw new RuntimeException(ex);
-				}
-			}
-		});
-		btnEdit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-					drawingArea.edit();
-
-			}
-		});
-
-		add(panelRight, BorderLayout.EAST);
-		add(panelLeft, BorderLayout.CENTER);
-		panelRight.setLayout(new BoxLayout(panelRight, BoxLayout.LINE_AXIS));
-		panelRight.add(btnTeach);
-		panelRight.add(btnLoad);
-		panelRight.add(btnEdit);
-		panelRight.add(btnSave);
-		panelRight.add(btnClear);
-		sliderSlices.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				JSlider source = (JSlider) e.getSource();
-				slices = source.getValue();
-				drawingArea.repaint();
-
-				System.out.println(slices);
-			}
-		});
 
 		/*
 		 //1 warstwa 1 neuron
@@ -143,7 +65,7 @@ public class Test extends JFrame{
 		tab[0]=10; tab[1]=1;
 		siec=new Siec(2,2,tab);
 		*/
-		setVisible(true);
+
 	}
 
 	public static void main(String[] args) {
@@ -151,7 +73,7 @@ public class Test extends JFrame{
 			
 			@Override
 			public void run() {
-				new Test("neurony");
+				new Test("TIP");
 			}
 		});
 	}
---
+++ src/UI.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+
+public class UI extends JFrame {
+        public UI() {
+            initialization();
+
+
+
+            JSlider sliderSlices = new JSlider(JSlider.HORIZONTAL, 8,64, 8);
+
+            JPanel panelRight = new JPanel();
+            JPanel panelLeft = new JPanel(new BorderLayout());
+            JPanel panelDrawing = new JPanel(new BorderLayout());
+            panelDrawing.setBounds(0, 0, 100, 100);
+            DrawArea drawingArea = new DrawArea();
+            drawingArea.setBounds(0,0,200,200);
+
+            panelDrawing.setBorder(new EmptyBorder(100, 100, 100, 100));
+            panelDrawing.add(drawingArea, BorderLayout.CENTER);
+            panelLeft.add(panelDrawing, BorderLayout.CENTER);
+            panelLeft.add(sliderSlices, BorderLayout.SOUTH);
+
+            JButton btnTeach = new JButton("Teach");
+            JButton btnSave = new JButton("Save");
+            JButton btnEdit = new JButton("Edit");
+            JButton btnLoad = new JButton("Load");
+            JButton btnClear = new JButton("Clear");
+            btnClear.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    drawingArea.clear();
+                }
+            });
+
+            btnSave.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        drawingArea.save();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
+
+            btnLoad.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        drawingArea.load();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
+            btnEdit.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    drawingArea.edit();
+
+                }
+            });
+
+            add(panelRight, BorderLayout.EAST);
+            add(panelLeft, BorderLayout.CENTER);
+            panelRight.setLayout(new BoxLayout(panelRight, BoxLayout.LINE_AXIS));
+            panelRight.add(btnTeach);
+            panelRight.add(btnLoad);
+            panelRight.add(btnEdit);
+            panelRight.add(btnSave);
+            panelRight.add(btnClear);
+            sliderSlices.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    JSlider source = (JSlider) e.getSource();
+                 //   slices = source.getValue();
+                    drawingArea.repaint();
+
+                    //System.out.println(slices);
+                }
+            });
+            setVisible(true);
+    }
+    private void initialization(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        Toolkit kit=Toolkit.getDefaultToolkit();
+        Dimension d=kit.getScreenSize();
+        setBounds(d.width/4,d.height/4,d.width/2,d.height/2);
+    }
+}
